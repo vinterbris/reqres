59704bf0098712fb077288f87b66d48e8a94de2e
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-25 21:07:51 +0300

    added negative tests for registration and fake data

--- reqres_tests/data.py
+++ reqres_tests/data.py
@@ -19,7 +19,7 @@ create_user_job = 'Carpenter'
 user_data = {"name": create_user_name, "job": create_user_job}
 
 register_user_data = {"email": "eve.holt@reqres.in", "password": "pistol"}
-register_user_data_incomplete = {"email": "eve.holt@reqres.in"}
+
 
 login_data = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
 login_data_incomplete = {"email": "eve.holt@reqres.in"}
--- tests/test_cas.py
+++ tests/test_cas.py
@@ -1,13 +1,14 @@
 from allure_commons._allure import step
+from faker import Faker
 
 from reqres_tests.data import (
     login_data,
-    register_user_data,
-    register_user_data_incomplete,
     login_data_incomplete,
 )
 from reqres_tests.utils.logger import send_reqres
 from reqres_tests.utils.schema import validate_schema
+
+fake = Faker()
 
 
 def test_login_successful():
@@ -44,36 +45,65 @@ def test_login_unsuccessful():
         validate_schema(body, "login_user_unsuccessful.json")
 
 
-def test_register_user_successful():
-    data = register_user_data
+class TestRegistration:
+    def test_register_user_successful(self):
+        password = fake.password()
+        data = {"email": "eve.holt@reqres.in", "password": password}
 
-    with step('Зарегистрировать пользователя'):
-        response = send_reqres('/api/register', 'post', json=data)
+        with step('Зарегистрировать пользователя'):
+            response = send_reqres('/api/register', 'post', json=data)
 
-    with step('Статус код == 200'):
-        assert response.status_code == 200
+        with step('Статус код == 200'):
+            assert response.status_code == 200
 
-    with step('Проверить корректность ответа'):
-        body = response.json()
-        assert body['id'] == 4
-        assert body['token'] == 'QpwL5tke4Pnpja7X4'
+        with step('Проверить корректность ответа'):
+            body = response.json()
+            assert body['id'] == 4
+            assert body['token'] == 'QpwL5tke4Pnpja7X4'
 
-    with step('Валидировать схему'):
-        validate_schema(body, "registration.json")
+        with step('Валидировать схему'):
+            validate_schema(body, "registration.json")
 
+    def test_register_user_without_password(self):
+        email = fake.email()
+        data = {"email": email}
 
-def test_register_user_unsuccessful():
-    data = register_user_data_incomplete
+        with step('Зарегистрировать пользователя'):
+            response = send_reqres('/api/register', 'post', json=data)
 
-    with step('Зарегистрировать пользователя'):
-        response = send_reqres('/api/register', 'post', json=data)
+        with step('Статус код == 400'):
+            assert response.status_code == 400
 
-    with step('Статус код == 400'):
-        assert response.status_code == 400
+        with step('Проверить корректность ответа'):
+            body = response.json()
+            assert body['error'] == "Missing password"
 
-    with step('Проверить корректность ответа'):
-        body = response.json()
-        assert body['error'] == "Missing password"
+        with step('Валидировать схему'):
+            validate_schema(body, "regsiter_user_unsuccessful.json")
 
-    with step('Валидировать схему'):
-        validate_schema(body, "regsiter_user_unsuccessful.json")
+    def test_register_user_without_login(self):
+        password = fake.password()
+        data = {"password": password}
+
+        with step('Зарегистрировать пользователя'):
+            response = send_reqres('/api/register', 'post', json=data)
+
+        with step('Статус код == 400'):
+            assert response.status_code == 400
+
+        with step('Проверить корректность ответа'):
+            body = response.json()
+            assert body['error'] == "Missing email or username"
+
+    def test_register_user_without_body(self):
+        data = {}
+
+        with step('Зарегистрировать пользователя'):
+            response = send_reqres('/api/register', 'post', json=data)
+
+        with step('Статус код == 400'):
+            assert response.status_code == 400
+
+        with step('Проверить корректность ответа'):
+            body = response.json()
+            assert body['error'] == "Missing email or username"
